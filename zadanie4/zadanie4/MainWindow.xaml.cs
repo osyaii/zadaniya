4d95c749ef06dad0605e72c7c9d1b1c91078012e
osyaii <oscarcancer@yandex.ru>
2024-10-18 15:23:32 +0500

    task4

---
+++ zadanie4/zadanie4/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace zadanie4
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            textField.FontSize = 10;
+            slider.Minimum = 10;
+            slider.Maximum = 120;
+        }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        { 
+            var slider = sender as Slider;
+            double value = slider.Value;
+            textField.FontSize = slider.Value;         
+        }
+
+        private void butt_Click(object sender, RoutedEventArgs e)
+        {
+            string texxt = textField.Text;
+            
+            
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            saveFileDialog1.RestoreDirectory = true;
+
+            
+
+            saveFileDialog1.ShowDialog();
+
+            if (saveFileDialog1.FileName != "")
+            {
+                System.IO.FileStream fs =
+                    (System.IO.FileStream)saveFileDialog1.OpenFile();
+
+
+                fs.Close();
+            }
+            File.WriteAllText(saveFileDialog1.FileName, texxt);
+
+        }
+    }
+}
